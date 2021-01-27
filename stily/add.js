ef3ee2bb5554d2b1dc278b18e25d26704a986aba
Eduard <noizighenya@gmail.com>
2021-01-27 17:28:36 +0200

    Верстка html css

---
+++ stily/add.js
@@ -0,0 +1,11 @@
+const name = "James";
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fname) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+sayHelloLinting("James");
